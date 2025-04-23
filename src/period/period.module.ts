fd6052ba9fd940fbabe1c44d1909eb58f0d34af0
UnknOownU <149084644+UnknOownU@users.noreply.github.com>
2025-04-23 20:40:57 +0200

    feat(period): add Period module for managing academic periods

---
+++ src/period/period.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { PeriodService } from './period.service';
+import { PeriodController } from './period.controller';
+import { PrismaModule } from '../prisma/prisma.module';
+
+@Module({
+  imports: [PrismaModule],
+  controllers: [PeriodController],
+  providers: [PeriodService],
+  exports: [PeriodService],
+})
+export class PeriodModule {} 
