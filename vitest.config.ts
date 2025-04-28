e8fe7e79f5a98b38eefd70945ad0a70933a5e9be
Messa <messaoud.houri@outlook.fr>
2025-04-28 21:41:51 +0200

    chore: update vitest config to include unit test files

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,8 +5,8 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'node',
-    include: ['**/*.e2e-vitest.ts'],
-    root: './test',
+    include: ['**/*.e2e-vitest.ts', '**/*.spec.ts'],
+    root: './',
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'html'],
