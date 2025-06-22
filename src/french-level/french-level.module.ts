24b809c274fffc2a2458eab4ad6754a1e09df620
UnknOownU <149084644+UnknOownU@users.noreply.github.com>
2025-06-22 19:53:44 +0200

    refactor(french-level): update module config

--- src/french-level/french-level.module.ts
+++ src/french-level/french-level.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
-import { FrenchLevelService } from './french-level.service';
 import { FrenchLevelController } from './french-level.controller';
+import { FrenchLevelService } from './french-level.service';
+import { FrenchLevelRepository } from './french-level.repository';
 import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule],
   controllers: [FrenchLevelController],
-  providers: [FrenchLevelService],
+  providers: [FrenchLevelService, FrenchLevelRepository],
   exports: [FrenchLevelService],
 })
-export class FrenchLevelModule {} 
+export class FrenchLevelModule {}
