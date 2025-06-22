efe01d68f2e79b099594a91bb395fc5fa15abdc6
UnknOownU <149084644+UnknOownU@users.noreply.github.com>
2025-06-22 19:54:10 +0200

    refactor(exit-reason): update module dependencies

--- src/exit-reason/exit-reason.module.ts
+++ src/exit-reason/exit-reason.module.ts
@@ -9,4 +9,4 @@ import { PrismaModule } from '../prisma/prisma.module';
   providers: [ExitReasonService],
   exports: [ExitReasonService],
 })
-export class ExitReasonModule {} 
+export class ExitReasonModule {}
